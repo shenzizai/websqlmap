b8ac6cbc0e9d075f7fb4a5fbda1ab586ca6efea2
shenzizai <1663502058@qq.com>
2018-09-29 20:21:40 +0800

    添加了删除任务功能 增加了上传文件扫描功能

--- app.py
+++ app.py
@@ -50,14 +50,7 @@ def write_data(taskid,data):
     conn.commit()
     return True
 
-app = Flask(__name__)
 
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-#用户方法
 
 #获取taskid
 def get_taskid():
@@ -65,60 +58,6 @@ def get_taskid():
     r=requests.get(url=url)
     taskid=json.loads(r.content).get('taskid')
     return taskid
-
-#sqlmapapi的删除任务,但是并没有使用
-# @app.route('/task/<taskid>/delete/')
-# def task_delete(taskid):
-#     url=api_server+'/task/%s/delete' %taskid
-#     r=requests.get(url=url)
-#     return  r.content
-
-#sqlmap 核心函数
-
-# @app.route('/option/<taskid>/list')
-# def option_list(taskid):
-#     url=api_server+'/option/%s/list' %taskid
-#     r=requests.get(url=url)
-#     return  r.content
-
-# @app.route("/option/<taskid>/get",methods=['GET','POST'])
-# def option_get(taskid):
-#     url= api_server+'/option/%s/get' %taskid
-#     r=requests.get(url=url)
-#     return  r.content
-
-# @app.route("/option/<taskid>/set",methods=['GET','POST'])
-# def option_set(taskid):
-#     url= api_server+'/option/%s/set' %taskid
-#     r=requests.get(url=url)
-#     return  r.content
-
-
-#开始扫描，主函数
-@app.route("/scan/newscan")
-@app.route("/scan/start",methods=['GET','POST'])
-def scan_start(taskid=None):
-    if request.method=='GET':
-        return render_template('scan.html')
-    if request.method=='POST':
-        formDatas = request.form
-        scan_url_list = formDatas['url']
-        scan_url_list=scan_url_list.split('\r\n')
-        print scan_url_list
-        if  len(scan_url_list[0])>0:
-            for scan_url in scan_url_list:
-                taskid = get_taskid()
-                url = api_server + '/scan/%s/start' % taskid
-                r = requests.post(url=url, data=json.dumps({'url': scan_url}), headers={'Content-Type': 'application/json'})
-                # engineid=json.loads(r.content).get('engineid')
-                current_time=datetime.datetime.today().strftime('%Y-%m-%d %H:%M:%S')
-                # sql="insert into scan(date,taskid,url,engineid,scan_status) values('%s','%s','%s','%s','running')" %(current_time,taskid,scan_url,int(engineid))
-                sql = "insert into scan(date,taskid,url,scan_status) values('%s','%s','%s','running')" % (current_time, taskid, scan_url)
-                db_exec(sql)
-                threading.Thread(target=handle_task,args=(taskid,)).start()
-            return '<script>alert(/ok/);window.location.href="/scan/list"</script>'
-        else:
-            return '<script>alert(/error url/);history.go(-1)</script>'
 
 #任务处理，数据入库
 def handle_task(taskid):
@@ -132,50 +71,78 @@ def handle_task(taskid):
         if scan_status =='terminated':
             write_status(taskid,scan_status)
             data=get_result(taskid)
-            # log=scan_log(taskid)
             write_data(taskid,data)
-            # write_log(taskid,log)
             break
 
-# @app.route("/scan/<taskid>/stop")
-# def scan_stop(taskid):
-#     url=api_server+'/scan/%s/stop' %taskid
-#     r=requests.get(url=url)
-#     return r.content
-#
-# @app.route("/scan/<taskid>/kill")
-# def scan_kill(taskid):
-#     url=api_server+'/scan/%s/kill' %taskid
-#     r=requests.get(url=url)
-#     return r.content
-
-@app.route("/scan/list")
-def show(cursor=None):
-    cursor=db_exec('select * from scan')
-    return render_template('list.html',cursor=cursor)
-
-
-
-# @app.route("/scan/<taskid>/status")
 def get_status(taskid):
     url = api_server + '/scan/%s/status' % taskid
     r = requests.get(url=url)
     scan_status=json.loads(r.content).get('status')
     return scan_status
 
-#获取扫描结果
-# @app.route('/task/<taskid>/data')
 def get_result(taskid):
     url=api_server+'/scan/%s/data' %taskid
     r=requests.get(url)
     return r.content
 
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/scan/del',methods=['POST'])
+def del_task():
+    formDatas = request.form
+    try:
+        taskid = formDatas['taskid']
+        if len(taskid)==16:
+            sql="delete from scan where taskid='%s'" %taskid
+            db_exec(sql)
+            return 'OK'
+    except Exception,e:
+        return e
+
+#开始扫描，主函数
+@app.route("/scan/newscan")
+@app.route("/scan/start",methods=['GET','POST'])
+def scan_start(taskid=None):
+    if request.method=='GET':
+        return render_template('scan.html')
+    if request.method=='POST':
+        if len(request.files)!=0:
+            f=request.files['file']
+            scan_url_list=f.read().split('\r\n')
+        else:
+            formDatas = request.form
+            scan_url_list = formDatas['url']
+            scan_url_list=scan_url_list.split('\r\n')
+        if  len(scan_url_list[0])>0:
+            for scan_url in scan_url_list:
+                taskid = get_taskid()
+                url = api_server + '/scan/%s/start' % taskid
+                r = requests.post(url=url, data=json.dumps({'url': scan_url}), headers={'Content-Type': 'application/json'})
+                current_time=datetime.datetime.today().strftime('%Y-%m-%d %H:%M:%S')
+                sql = "insert into scan(date,taskid,url,scan_status) values('%s','%s','%s','running')" % (current_time, taskid, scan_url)
+                db_exec(sql)
+                threading.Thread(target=handle_task,args=(taskid,)).start()
+            return '<script>alert(/ok/);window.location.href="/scan/list"</script>'
+        else:
+            return '<script>alert(/error url/);history.go(-1)</script>'
+
+@app.route("/scan/list")
+def show(cursor=None):
+    cursor=db_exec('select * from scan')
+    return render_template('list.html', cursor=cursor)
+
 @app.route('/scan/succ')
 def get_succ(cursor=None):
     cursor=db_exec("select * from scan where scan_status='terminated' and length(data)!=58")
-    return render_template('list.html',cursor=cursor)
-
-
+    return render_template('list.html', cursor=cursor)
 
 if __name__ == '__main__':
     app.debug = True
